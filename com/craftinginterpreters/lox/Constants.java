d4738c3c11e7a24226825def33d15b3f7cfe19d5
Matthew Watt <matt@twopoint.dev>
2021-11-16 20:04:34 -0600

    Finish ch7 - evaluating expressions

--- com/craftinginterpreters/lox/Constants.java
+++ com/craftinginterpreters/lox/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     class ExitCodes {
         static final int EX_USAGE = 64;
         static final int EX_DATAERR = 65;
+        static final int EX_SOFTWARE = 70;
     }
 }
